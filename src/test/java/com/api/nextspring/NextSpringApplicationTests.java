3dc5b6233a483bc6e67d1d08edddbf19bf655625
Athirson Silva <Athirsonarceus@gmail.com>
2023-02-27 11:59:43 -0300

    Initial commit

---
+++ src/test/java/com/api/nextspring/NextSpringApplicationTests.java
@@ -0,0 +1,13 @@
+package com.api.nextspring;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class NextSpringApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
