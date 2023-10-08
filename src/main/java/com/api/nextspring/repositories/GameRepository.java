4c6516f47ad9de29aa9eefac6f5548cc82d2a35f
Athirson Silva <Athirsonarceus@gmail.com>
2023-10-08 15:11:14 -0300

    adding repositories javadoc documentation

--- src/main/java/com/api/nextspring/repositories/GameRepository.java
+++ src/main/java/com/api/nextspring/repositories/GameRepository.java
@@ -13,11 +13,45 @@ import org.springframework.data.jpa.repository.Query;
 import com.api.nextspring.config.RedisConfiguration;
 import com.api.nextspring.entity.GameEntity;
 
+/**
+ * This interface represents the repository for GameEntity objects. It extends
+ * the JpaRepository interface
+ * and provides additional methods for searching and caching game entities.
+ * 
+ * @see JpaRepository
+ * @see GameEntity
+ * @see RedisConfiguration
+ * @see CacheAutoConfiguration
+ * @see Cacheable
+ * 
+ * @implNote This interface represents the repository for GameEntity objects. It
+ *           extends the JpaRepository interface
+ * 
+ * @author Athirson Silva
+ */
 @ImportAutoConfiguration(classes = { RedisConfiguration.class, CacheAutoConfiguration.class })
 public interface GameRepository extends JpaRepository<GameEntity, UUID> {
+	/**
+	 * Checks if a game with the given name exists in the cache.
+	 *
+	 * @param name the name of the game to check
+	 * @return true if a game with the given name exists in the cache, false
+	 *         otherwise
+	 */
 	@Cacheable(value = "game", key = "#name")
 	boolean existsByName(String name);
 
+	/**
+	 * Searches for game entities that match the given query string. The search is
+	 * performed on the name, description,
+	 * grade, genre name, and genre description fields of the game entities. The
+	 * search is case-insensitive and partial
+	 * matches are allowed.
+	 *
+	 * @param query    the query string to search for
+	 * @param pageable the pagination information
+	 * @return a page of game entities that match the given query string
+	 */
 	@Query("SELECT g FROM GameEntity g WHERE " +
 			"LOWER(g.name) LIKE LOWER(CONCAT('%', :query, '%'))" +
 			"OR LOWER(g.description) LIKE LOWER(CONCAT('%', :query, '%'))" +
@@ -27,5 +61,11 @@ public interface GameRepository extends JpaRepository<GameEntity, UUID> {
 	@Cacheable(value = "game", key = "#query")
 	Page<GameEntity> searchGameEntities(String query, Pageable pageable);
 
+	/**
+	 * Returns a page of all games in the repository.
+	 *
+	 * @param pageable the pagination information
+	 * @return a page of game entities
+	 */
 	Page<GameEntity> findAll(Pageable pageable);
 }
--- src/main/java/com/api/nextspring/repositories/RoleRepository.java
+++ src/main/java/com/api/nextspring/repositories/RoleRepository.java
@@ -11,8 +11,32 @@ import org.springframework.data.jpa.repository.JpaRepository;
 import com.api.nextspring.config.RedisConfiguration;
 import com.api.nextspring.entity.RoleEntity;
 
+/**
+ * This interface represents the repository for RoleEntity, which extends
+ * JpaRepository.
+ * It provides methods to interact with the database and cache.
+ * 
+ * @see JpaRepository
+ * @see RoleEntity
+ * @see RedisConfiguration
+ * @see CacheAutoConfiguration
+ * @see Cacheable
+ * 
+ * @implNote This interface represents the repository for RoleEntity, which
+ *           extends JpaRepository.
+ *           It provides methods to interact with the database and cache.
+ * 
+ * @author Athirson Silva
+ */
 @ImportAutoConfiguration(classes = { RedisConfiguration.class, CacheAutoConfiguration.class })
 public interface RoleRepository extends JpaRepository<RoleEntity, UUID> {
+	/**
+	 * This method finds a RoleEntity by its name from the cache or database.
+	 * 
+	 * @param name the name of the role to find
+	 * @return an Optional containing the RoleEntity if found, otherwise an empty
+	 *         Optional
+	 */
 	@Cacheable(value = "role", key = "#name")
 	Optional<RoleEntity> findByName(String name);
 }
