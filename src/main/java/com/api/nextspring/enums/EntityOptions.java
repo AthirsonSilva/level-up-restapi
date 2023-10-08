4bf0dffb22a199c674a04749285d77859edd3fc7
Athirson Silva <Athirsonarceus@gmail.com>
2023-10-08 15:23:33 -0300

    adding enums javadoc documentation

--- src/main/java/com/api/nextspring/enums/EntityOptions.java
+++ src/main/java/com/api/nextspring/enums/EntityOptions.java
@@ -1,5 +1,25 @@
 package com.api.nextspring.enums;
 
+import com.api.nextspring.entity.DeveloperEntity;
+import com.api.nextspring.entity.GameEntity;
+import com.api.nextspring.entity.GenreEntity;
+import com.api.nextspring.entity.UserEntity;
+
+/**
+ * This enum represents the different entity options available in the
+ * application.
+ * It includes options for Game, Genre, Developer, and User.
+ * 
+ * @see {@link GameEntity} the Game entity class, representing the GAME enum
+ * @see {@link GenreEntity} the Genre entity class, representing the GENRE enum
+ * @see {@link DeveloperEntity} the Developer entity class, representing the
+ *      DEVELOPER enum
+ * @see {@link UserEntity} the User entity class, representing the USER enum
+ * 
+ * @param value The string value of the enum.
+ * 
+ * @author Athirson Silva
+ */
 public enum EntityOptions {
 	GAME("Game"),
 	GENRE("Genre"),
@@ -12,6 +32,11 @@ public enum EntityOptions {
 		this.value = value;
 	}
 
+	/**
+	 * Returns the string value of the enum.
+	 * 
+	 * @return the string value of the enum
+	 */
 	public String getValue() {
 		return value;
 	}
